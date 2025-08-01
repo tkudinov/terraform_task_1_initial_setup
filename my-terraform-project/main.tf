f1520509ae87fe2351b6a446962fd135c8c00268
tkudinov <tkudinov90@gmail.com>
2025-08-01 14:24:10 +0300

    Changed resource group name.

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "mate-resource_tk"
+  name                     = "mate_resource_tk"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
